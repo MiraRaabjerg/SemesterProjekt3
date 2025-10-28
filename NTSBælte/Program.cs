fda30c8334e36544dd9dc759cd28c0da87452ce8
Ida Bengtsen <ida.ishoejbengtsen@gmail.com>
2025-10-28 08:44:04 +0100

    Implementering af MAUI-app

--- NTSBælte/Program.cs
+++ NTSBælte/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using System.Threading;
+using DataB;
+
+class Program
+{
+    static void Main()
+    {
+        int dataPin = 5;   // GPIO-pin til DOUT
+        int clockPin = 6;  // GPIO-pin til SCK
+
+        using var ds = new DSRespiration(dataPin, clockPin);
+
+        Console.WriteLine("Starter måling...\n");
+        while (true)
+        {
+            double v = ds.LæsSignal();
+            Thread.Sleep(200);   // 5 målinger i sekundet
+        }
+    }
+}
