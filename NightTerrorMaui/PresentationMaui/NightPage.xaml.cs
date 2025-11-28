d6c7567b637330e728b8a54e66d62e264c27303b
Ida Bengtsen <ida.ishoejbengtsen@gmail.com>
2025-11-28 12:13:16 +0100

    Ny version med xaml.cs

--- NightTerrorMaui/PresentationMaui/NightPage.xaml.cs
+++ NightTerrorMaui/PresentationMaui/NightPage.xaml.cs
@@ -1,14 +1,34 @@
-﻿namespace NightTerrorMaui.PresentationMaui
+﻿using Microsoft.Maui.Dispatching;
+using NightTerrorMaui.PresentationMaui;
+
+namespace NightTerrorMaui.PresentationMaui
 {
     public partial class NightPage : ContentPage
     {
+        private readonly NightViewModel _vm;
+
         public NightPage(NightViewModel vm)
         {
             InitializeComponent();
+
+            _vm = vm;
             BindingContext = vm;
+
+            // Når VM siger "Chart ændret", så invaliderer vi grafen
+            _vm.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(NightViewModel.Chart))
+                {
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        ChartView.Invalidate();   // tving GraphicsView til at tegne igen
+                    });
+                }
+            };
         }
     }
 }
+
 
 
 
